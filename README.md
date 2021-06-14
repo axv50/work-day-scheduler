0d3e6c03379ff142e0650bb6c540714c385d4b8b
axv50 <alexvega50@gmail.com>
2021-06-13 23:08:41 -0500

    fixed file load

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Work Day Scheduler Starter Code
+title: work-day-scheduler
+description: planner for your work day
+author: Alexis Vega
+created: 6/13/21
+
+Work Day Scheduler
+
+Readme
+
+Description:
--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+//Date and day//
 var currentDate = moment().format('dddd, MMMM Do, YYYY')
 
 $("#currentDay").text(currentDate);
 
 var currentHour = moment().hour();
+
+
 
 $(".blockHour").each(function() {
     var blockHour = $(this).attr("id");
@@ -24,3 +27,18 @@ $(".blockHour").each(function() {
     $(this).removeClass("present")
 }
 })
+
+//local storage//
+
+$("#9 .blockHour").val(localStorage.getItem("9"));
+$("#10 .blockHour").val(localStorage.getItem("10"));
+$("#11 .blockHour").val(localStorage.getItem("11"));
+$("#12 .blockHour").val(localStorage.getItem("12"));
+$("#1 .blockHour").val(localStorage.getItem("1"));
+$("#2 .blockHour").val(localStorage.getItem("2"));
+$("#3 .blockHour").val(localStorage.getItem("3"));
+$("#4 .blockHour").val(localStorage.getItem("4"));
+$("#5 .blockHour").val(localStorage.getItem("5"));
+
+
+timeTracker();
